a8b679d9de6ccf074e4deeead57218974bf6d6ac
Ajay Kumar Barun <66874151+ajbarun@users.noreply.github.com>
2020-12-01 18:05:02 +0530

    Update Healthcheck.cs

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -7,7 +7,7 @@ namespace poi.Models
     {
         public Healthcheck()
         {
-            Message = "POI Service Healthcheck Pass";
+            Message = "POI Service Healthcheck";
             Status = "Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
